e36e7888efed1a2650af1455f7eabefa98fdf7a8
Irfan <irfanalamt@gmail.com>
2022-07-05 11:22:46 -0400

    Feat: conditional render appbar;add signout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Typography, AppBar, Toolbar, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const LogoLg = styled(Typography)(({ theme }) => ({
   display: 'none',
@@ -13,6 +15,12 @@ const LogoSm = styled(Typography)(({ theme }) => ({
 }));
 
 const Navbar = () => {
+  const { data: session, status } = useSession();
+  const handleSignout = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+
   return (
     <AppBar position='static' sx={{ backgroundColor: '#3f51b5', paddingY: 1 }}>
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -22,29 +30,45 @@ const Navbar = () => {
         <LogoSm color='black' variant='h6'>
           DAT
         </LogoSm>
-        <div style={{ alignItems: 'center' }}>
+        {!session && (
+          <div style={{ alignItems: 'center' }}>
+            <Button
+              variant='contained'
+              href='/login'
+              sx={{
+                color: 'white',
+                backgroundColor: '#00897A',
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              variant='contained'
+              href='/register'
+              sx={{
+                color: 'white',
+                marginX: '10px',
+                backgroundColor: '#00897A',
+              }}
+            >
+              Register
+            </Button>
+          </div>
+        )}
+
+        {session && (
           <Button
             variant='contained'
-            href='/login'
-            sx={{
-              color: 'white',
-              backgroundColor: '#00897A',
-            }}
-          >
-            Login
-          </Button>
-          <Button
-            variant='contained'
-            href='/register'
+            onClick={handleSignout}
             sx={{
               color: 'white',
               marginX: '10px',
               backgroundColor: '#00897A',
             }}
           >
-            Register
+            <LogoutIcon sx={{ marginX: 1 }} /> Sign Out
           </Button>
-        </div>
+        )}
       </Toolbar>
     </AppBar>
   );
