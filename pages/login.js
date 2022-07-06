8bf560f664c87e02f90d6c64fbbd54f7a2e68de3
Irfan <irfanalamt@gmail.com>
2022-07-06 14:02:13 -0400

    Feat: add return btn; show toast on loginError

--- pages/login.js
+++ pages/login.js
@@ -10,11 +10,15 @@ import {
   Typography,
 } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
+import HomeIcon from '@mui/icons-material/Home';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Image from 'next/image';
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
   const { data: session } = useSession();
@@ -40,110 +44,136 @@ export default function Login() {
         password,
       });
       if (result.error) {
-        alert(result.error);
+        toast.error(`${result.error}`, {
+          position: 'top-center',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       }
     } catch (err) {
-      alert(getError(err));
+      toast.error(`${getError(err)}`, {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <Image
-        style={{ opacity: '0.5', zIndex: -1 }}
-        src='/images/back-pattern.jpg'
-        alt=''
-        layout='fill'
-        objectFit='cover'
-        objectPosition='center'
-      />
-      <Paper
-        sx={{ padding: '15px', paddingX: '15px', marginTop: '30px' }}
-        elevation={3}
-      >
-        <Box
-          sx={{
-            marginTop: 2,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: 2,
-          }}
+    <>
+      <Container component='main' maxWidth='xs'>
+        <Image
+          style={{ opacity: '0.5', zIndex: -1 }}
+          src='/images/back-pattern.jpg'
+          alt=''
+          layout='fill'
+          objectFit='cover'
+          objectPosition='center'
+        />
+        <Button sx={{ marginTop: 2 }} variant='contained' href='/'>
+          <ArrowBackIcon />
+          <HomeIcon />
+        </Button>
+        <Paper
+          sx={{ padding: '15px', paddingX: '15px', marginTop: 2 }}
+          elevation={3}
         >
-          <Typography variant='h4' component='div'>
-            DAT login
-          </Typography>
-          <Avatar
+          <Box
             sx={{
-              m: 1,
-              bgcolor: 'secondary.main',
-              paddingRight: '8px',
+              marginTop: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              padding: 2,
             }}
           >
-            <LoginIcon />
-          </Avatar>
-          <Box
-            component='form'
-            noValidate
-            onSubmit={handleSubmit(submitHandler)}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id='email'
-                  label='Email Address'
-                  name='email'
-                  {...register('email', {
-                    required: 'Please enter email',
-                    pattern: {
-                      value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                      message: 'Please enter valid email',
-                    },
-                  })}
-                  error={!!errors?.email}
-                  helperText={errors?.email ? errors.email.message : null}
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                  {...register('password', {
-                    required: 'Please enter password',
-                    minLength: {
-                      value: 3,
-                      message: 'password length should be greater than 3',
-                    },
-                  })}
-                  error={!!errors?.password}
-                  helperText={errors?.password ? errors.password.message : null}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2, backgroundColor: '#00897A' }}
+            <Typography variant='h4' component='div'>
+              DAT login
+            </Typography>
+            <Avatar
+              sx={{
+                m: 1,
+                bgcolor: 'secondary.main',
+                paddingRight: '8px',
+              }}
             >
-              Login
-            </Button>
+              <LoginIcon />
+            </Avatar>
+            <Box
+              component='form'
+              noValidate
+              onSubmit={handleSubmit(submitHandler)}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id='email'
+                    label='Email Address'
+                    name='email'
+                    {...register('email', {
+                      required: 'Please enter email',
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                        message: 'Please enter valid email',
+                      },
+                    })}
+                    error={!!errors?.email}
+                    helperText={errors?.email ? errors.email.message : null}
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name='password'
+                    label='Password'
+                    type='password'
+                    id='password'
+                    {...register('password', {
+                      required: 'Please enter password',
+                      minLength: {
+                        value: 3,
+                        message: 'password length should be greater than 3',
+                      },
+                    })}
+                    error={!!errors?.password}
+                    helperText={
+                      errors?.password ? errors.password.message : null
+                    }
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2, backgroundColor: '#00897A' }}
+              >
+                Login
+              </Button>
+            </Box>
+            <Grid container justifyContent='flex-end'>
+              <Button variant='text' size='small' href='/register'>
+                New user? Register
+              </Button>
+            </Grid>
           </Box>
-          <Grid container justifyContent='flex-end'>
-            <Button variant='text' size='small' href='/register'>
-              New user? Register
-            </Button>
-          </Grid>
-        </Box>
-      </Paper>
-    </Container>
+        </Paper>
+        <ToastContainer />
+      </Container>
+    </>
   );
 }
